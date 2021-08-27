f406e32888cae5ec45572602088f84fd6fb765b1
GabrielPassos25 <gabrielurano30@gmail.com>
2021-08-27 11:43:28 -0300

    [:memo:] Sdding documentation of styles and standard application architecture

--- App.tsx
+++ App.tsx
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import { useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold} from '@expo-google-fonts/poppins'
+import AppLoading from 'expo-app-loading';
+import theme from './src/styles/theme';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false,  animationEnabled: false}}>
+          {/* <Stack.Screen name="Login" component={Login}/> */}
+          {/* <Stack.Screen name="Home" component={Home}/> */}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+export default {
+    colors: {
+      primary: "#F46F0A",
+      primary_dark: "#BD5800",
+      gray: "#7B7B7B",
+      white: "#FFFFFF",
+      linear_gradient_background: ["#F46F0A","#D86305","#BD5800"],
+    },
+    fonts: {
+      regular: "Poppins_400Regular",
+      medium: "Poppins_500Medium",
+      bold: "Poppins_700Bold",
+    },
+  };
+  
